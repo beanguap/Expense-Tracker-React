afe5fa263931b4a9d97344d69f0c86a4029b947a
Jeriel Martinez <martinez.jeriel01@gmail.com>
2024-07-03 18:33:42 -0400

    expenses

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import { MainLayout } from "./styles/Layout";
 import Orb from "./components/Orb/Orb";
 import Navigation from "./components/Navigation/Navigation";
 import { useMemo, useState } from "react";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Income from "./components/Incomes/Income";
+import Expenses from "./components/Expenses/Expenses"
 
 function App() {
   const [active, setActive] = useState(1)
@@ -12,6 +15,13 @@ function App() {
     switch(active){
       case 1:
         return <Dashboard />
+      case 2: 
+        return <Dashboard />
+      case 3:
+        return <Income />
+      case 4: 
+        return <Expenses />
+      default: { <Dashboard />}
     }
   }
 
