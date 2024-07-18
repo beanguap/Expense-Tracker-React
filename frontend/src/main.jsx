6ce845f94d0d47979d784a6796bac76f4728bc81
Jeriel Martinez <martinez.jeriel01@gmail.com>
2024-07-18 15:11:32 -0400

    fix main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom';
 import App from './App';
-import { GlobalProvider } from './context/globalContext';
+import { GlobalProvider } from './Context/globalContext.jsx';
 import { GlobalStyle } from './styles/GlobalStyle';
 
 const root = createRoot(document.getElementById('root'));
